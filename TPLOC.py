7016e7ca6dea632302c8232b35f2ce607734ecd5
Mathilde H <hannequinmathilde@gmail.com>
2026-02-11 16:40:51 +0100

    Ajout du Kmeans

--- TPLOC.py
+++ TPLOC.py
@@ -1,4 +1,7 @@
 import os
+import numpy as np
+from PIL import Image
+from sklearn.cluster import KMeans
 from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
@@ -48,6 +51,41 @@ def galerie():
 def image_brute(nom_image):
     dossier = request.args.get('dossier')
     return send_from_directory(dossier, nom_image)
+
+@app.route('/segmentation')
+def segmentation():
+    chemin_dossier = request.args.get('chemin')
+    nom_image = request.args.get('image_selectionnee')
+    k_clusters = request.args.get('k', 5, type=int) 
+    
+    image_segmentee_url = None
+
+    if chemin_dossier and nom_image:
+        chemin_complet = os.path.join(chemin_dossier, nom_image)
+        
+        img = Image.open(chemin_complet).convert('RGB')
+        img_np = np.array(img)
+        h, w, c = img_np.shape
+        
+        pixels = img_np.reshape(-1, c)
+        print(pixels.shape)
+        
+        kmeans = KMeans(n_clusters=k_clusters, n_init=10)
+        labels = kmeans.fit_predict(pixels)
+        
+        centres = kmeans.cluster_centers_.astype(np.uint8)
+        img_reconstruite = centres[labels].reshape(h, w, c)
+        
+        res_pil = Image.fromarray(img_reconstruite)
+        nom_fichier_resultat = "temp_segmentation.png"
+        res_pil.save(os.path.join('static', nom_fichier_resultat))
+        image_segmentee_url = nom_fichier_resultat
+
+    return render_template('segmentation.html', 
+                           chemin=chemin_dossier, 
+                           nom_image=nom_image, 
+                           k=k_clusters,
+                           resultat=image_segmentee_url)
 
 
 if __name__ == '__main__':
